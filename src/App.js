a2446e22ca19a5a785a3fa569dd85d364b07c20a
Pinchon Léo <leopinchonspa@gmail.com>
2022-03-16 21:27:06 +0100

    ajout voiture + map

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import "./App.css";
+import Loader from './Components/Loader.js';
 import { Routes } from "react-router-dom";
+import { useState, useEffect } from 'react'
+
+
+import Game2 from "./Screens/Game2/Game2";
 
 function App() {
+
+  let [isLoaded, setIsLoaded] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoaded(false)
+    }, 0)
+  }, [])
+
   return (
+    <>
+      {isLoaded === false ? (
+
     <div className="App">
       <header className="App-header"></header>
+  
+      <Game2/>
+
       <Routes></Routes>
-    </div>
-  );
+    </div>) : (
+    <Loader />
+    )}
+  </>
+  )
 }
 
 export default App;
