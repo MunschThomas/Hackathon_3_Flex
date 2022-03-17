f6945ad4425875637fdb775cb35ae70823761388
Pinchon Léo <leopinchonspa@gmail.com>
2022-03-17 23:28:45 +0100

    fin de partie ok

--- src/Components/ModalQ.js
+++ src/Components/ModalQ.js
@@ -1,62 +1,103 @@
-// import dataQuestions from '../assets/fondamentaux.json'
-import { useEffect, useState } from 'react'
-import './ModalQ.css'
+import { useEffect, useState, useRef } from "react";
+import "./ModalQ.css";
 
-const ModalQ = ({ dataQuestion, setDataQuestion }) => {
-  const [isAsked, setIsAsked] = useState([])
-  const [clk, setClk] = useState(false)
+const ModalQ = ({ pause, dataQuestion, setDataQuestion, launchNewGame , rightAnswer, setRightAnswer}) => {
+  const focus1 = useRef(null)
+  const [isAsked, setIsAsked] = useState([]);
+  const [clk, setClk] = useState(false);
 
-  const [answerCorrect, setAnswerCorrect] = useState(null)
-  const [selectedAnswer, setSelectedAnswer] = useState(null)
+
+  // const onFoc = () => {
+  //   focus.current.focus()
+
+  // }
+
+  const [answerCorrect, setAnswerCorrect] = useState(null);
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+  
+
+  const checkKey = (choice) => {
+    focus1.current.focus()
+    setSelectedAnswer(choice);
+    if (choice === isAsked.reponse) {
+      const showTrue = document.getElementById("correctAnswerReavel");
+      showTrue.style.visibility = "visible";
+      setRightAnswer((rightAnswer) => rightAnswer + 1)
+    } else {
+      const showTrue = document.getElementById("wrongAnswerReavel");
+      showTrue.style.visibility = "visible";
+    }
+  };
+  const launch = (e) => {
+    console.log("keyboardEvent", e);
+    // let wrapper = document.getElementById("wrapper");
+
+    if (e.key === " ") {
+      launchNewGame();
+    }
+    if (e.key === "ArrowLeft") {
+      checkKey("Vrai");
+      console.log('efefef');
+    }
+    if (e.key === "ArrowRight") {
+      checkKey("Faux");
+    }
+  };
+
+  useEffect(() => {console.log(rightAnswer); },[rightAnswer])
 
   const handleListItemClick = (event) => {
-    console.log('CONTENT', event.target.textContent)
-    setSelectedAnswer(event.target.textContent)
+    console.log("CONTENT", event.target.textContent);
+    setSelectedAnswer(event.target.textContent);
     if (event.target.textContent === isAsked.reponse) {
-      const showTrue = document.getElementById('correctAnswerReavel')
-      showTrue.style.visibility = 'visible'
+      const showTrue = document.getElementById("correctAnswerReavel");
+      showTrue.style.visibility = "visible";
+      setRightAnswer(rightAnswer => rightAnswer + 1)
     } else {
-      const showTrue = document.getElementById('wrongAnswerReavel')
-      showTrue.style.visibility = 'visible'
+      const showTrue = document.getElementById("wrongAnswerReavel");
+      showTrue.style.visibility = "visible";
     }
-  }
+  };
   useEffect(() => {
-    const quest = Math.floor(Math.random() * dataQuestion.length)
+    const quest = Math.floor(Math.random() * dataQuestion.length);
 
-    console.log(dataQuestion)
+    console.log("dataQuestion", dataQuestion, "quest", quest);
 
     const chosenQuest = dataQuestion.filter(
-      (dataQuestion) => dataQuestion.id === quest
-    )
-    const newTab = dataQuestion.filter((el) => el.id !== chosenQuest[0].id)
-    console.log('newTab', newTab)
-    setDataQuestion(newTab)
-    setIsAsked(...chosenQuest)
-  }, [clk])
+      (dataQuestion, i) => i !== quest
+    );
+    const newTab = dataQuestion.filter((el) => el.id !== chosenQuest[0].id);
+    console.log("newTab", newTab);
+    setDataQuestion(newTab);
+    setIsAsked(...chosenQuest);
+
+    
+  }, [clk]);
 
   useEffect(() => {
-    console.log('in use effext', dataQuestion)
-  }, [dataQuestion])
+    console.log("in use effext", dataQuestion);
+  }, [dataQuestion]);
 
   return (
-    <div className='wrapper'>
-      <div className='wrapQuest'>
+    <div ref={focus1} className="wrapper" id="wrapper" tabIndex="0" onKeyDown={(e) => launch(e)}>
+      <div className="wrapQuest">
         <h1>{isAsked.question}</h1>
-        <div className='AnswerButton'>
-          <button onClick={handleListItemClick}>Vrai</button>
-          <button onClick={handleListItemClick}>Faux</button>
+        <div className="AnswerButton">
+          <button onClick={(e) => handleListItemClick(e)}>Vrai</button>
+          <button onClick={(e) => handleListItemClick(e)}>Faux</button>
         </div>
-        <div className='ReavelCorrect'>
-          <div id='correctAnswerReavel' className='AnswerReavel'>
+        <div className="ReavelCorrect">
+          <div id="correctAnswerReavel" className="AnswerReavel">
             <h2>Bravo</h2>
           </div>
-          <div id='wrongAnswerReavel' className='AnswerReavel'>
+          <div id="wrongAnswerReavel" className="AnswerReavel">
             <h2>Oh oh, {isAsked.correction}</h2>
           </div>
+          <button className={selectedAnswer ? `visible` : `hidden`} onClick={() => launchNewGame()}>Continuer</button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ModalQ
+export default ModalQ;
