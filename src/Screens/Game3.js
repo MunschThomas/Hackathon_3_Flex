ea9cb3e1af0da1bbd7bc9a5dba4400d42726e24d
Yannick Lanteri <yannick-lanteri_student2021@wilder.school>
2022-03-17 01:58:40 +0100

    add random obs and letters

--- src/Screens/Game3.js
+++ src/Screens/Game3.js
@@ -15,11 +15,12 @@ export default function Game2() {
   const [letters, setLetters] = useState([]);
   const [idNum, setIdNum] = useState(0);
   const [winLetters, setWinLetters] = useState(false);
+  const [pause, setPause] = useState(false);
 
   const newTableauNumber = [];
 
   useEffect(() => {
-    let tableauNumber = ["B", "n", "e", "d", "i", "s"];
+    let tableauNumber = [persos, persos, persos, persos, persos, persos];
     for (let i = 0; i < 6; i++) {
       const random = Math.floor(Math.random() * tableauNumber.length);
       const aEnlever = tableauNumber[random];
@@ -39,10 +40,76 @@ export default function Game2() {
     setIdNum((idNum) => idNum + 1);
   };
 
+  // *********** RAJOUTS OBSTACLES ALEATOIRES THOM YAN UP*****************
+
+  // USE EFFECT WITH OBSTACLES
+  useEffect(() => {
+    let idObs = 0;
+    let randomPos = Math.floor(Math.random() * place.length);
+    let randomObs = Math.floor(Math.random() * obstacle.length);
+    if (!winLetters && !pause) {
+      const interval = setInterval(() => {
+        console.log("letters", letters);
+        const obs = document.getElementById("obstacle");
+        const object = document.createElement("img");
+        object.setAttribute("src", obstacle[randomObs]);
+        object.setAttribute("alt", "rocks paper cisor");
+        object.setAttribute(
+          "class",
+          `obstacle ${place[randomPos]} obstacleDOM`
+        );
+        object.setAttribute("id", `obs${idObs}`);
+        obs.appendChild(object);
+        idObs += 1;
+        randomPos = Math.floor(Math.random() * place.length);
+        randomObs = Math.floor(Math.random() * obstacle.length);
+        // console.log("add", document.getElementById(`obs${idObs}`));
+        // console.log("delete", document.getElementById(`obs${idObs - 1}`));
+        const deletel = document.getElementById(`obs${idObs - 2}`);
+        if (idObs !== 0 && idObs !== 1) {
+          obs.removeChild(deletel);
+        }
+      }, 3500);
+      return () => clearInterval(interval);
+    }
+  }, [winLetters, pause]);
+
+  // USE EFFECT WITH LETTERS !!!!
+  useEffect(() => {
+    let idObs = 0;
+    let randomPos = Math.floor(Math.random() * place.length);
+    let randomLetter = Math.floor(Math.random() * letters.length);
+    if (letters.length !== 0 && !winLetters && !pause) {
+      const interval = setInterval(() => {
+        const obs = document.getElementById("obstacle");
+        const object = document.createElement("img");
+        object.setAttribute("src", letters[randomLetter]);
+        object.setAttribute("alt", "letter");
+        object.setAttribute(
+          "class",
+          `obstacle ${place[randomPos]} obstacleDOM`
+        );
+        object.setAttribute("id", `obs${idObs}`);
+        obs.appendChild(object);
+        idObs += 1;
+        randomPos = Math.floor(Math.random() * place.length);
+        randomLetter = Math.floor(Math.random() * letters.length);
+        // console.log("add", document.getElementById(`obs${idObs}`));
+        // console.log("delete", document.getElementById(`obs${idObs - 1}`));
+        const deletel = document.getElementById(`obs${idObs - 2}`);
+        if (idObs !== 0 && idObs !== 1) {
+          obs.removeChild(deletel);
+        }
+      }, 3500);
+      return () => clearInterval(interval);
+    }
+  }, [winLetters, pause, letters]);
+
   // *********** RAJOUTS THOM YAN DOWN*****************
 
   const place = ["left", "middle", "right"];
   const obstacle = [rocks, cat, tree];
+  const obstacle2 = ["lettre"];
 
   const [carPos, setCarPos] = useState(0);
 
@@ -66,22 +133,30 @@ export default function Game2() {
   //   console.log(document.getElementById("car").getBoundingClientRect());
   // }, []);
 
-  //  useEffect(()=> {
-
-  //    let road = document.getElementById('road').animate(
-  //      [{
-  //        backgroundPositionY: 0
-  //      },{
-  //        backgroundPositionY:100
-  //      }],
-  //      {duration: 3000,
-  //    ease: "linear",
-  //    iterations: Infinity
-  //    }
-
-  //    )
-  //  }
-  //  , [])
+  let road = () =>
+    document.getElementById("road").animate(
+      [
+        {
+          backgroundPositionY: 0,
+        },
+        {
+          backgroundPositionY: "1445px",
+        },
+      ],
+      {
+        duration: 3000,
+        ease: "linear",
+        iterations: Infinity,
+        forwards: true,
+      }
+    );
+  useEffect(() => {
+    if (!pause && !winLetters) {
+      road();
+    } else {
+      road().pause();
+    }
+  }, [pause, winLetters]);
 
   return (
     <>
@@ -93,34 +168,19 @@ export default function Game2() {
           onKeyDown={(e) => setMove(e)}
         >
           {/* // *********** RAJOUTS THOM YAN UP******************/}
-          {letters && letters[idNum]}
+          {/* {letters && letters[idNum]} */}
           {!winLetters ? (
-            <div onClick={() => newLetter()}>CLIQUE ICI</div>
+            <>
+              <div onClick={() => newLetter()}>CLIQUE ICI</div>
+              <div onClick={() => setPause(!pause)}>PAUSE</div>
+            </>
           ) : (
             <div>Victoire</div>
           )}
 
           {/* // *********** RAJOUTS THOM YAN DOWN******************/}
           <img src={persos} id="car" alt="perso" className="perso" />
-          {inGame && (
-            <div>
-              <img
-                src={obstacle[0]}
-                alt="rocks"
-                className={`obstacle ${place[0]}`}
-              />
-              <img
-                src={obstacle[1]}
-                alt="cat"
-                className={`obstacle ${place[1]}`}
-              />
-              <img
-                src={obstacle[2]}
-                alt="tree"
-                className={`obstacle ${place[2]}`}
-              />
-            </div>
-          )}
+          {inGame && <div id="obstacle"></div>}
         </div>
       </div>
     </>
